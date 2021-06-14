4bb477ca37b2aa9c8941caaafe7b073508a80b15
Edgar Jr <edgarpinheiro13@gmail.com>
2021-06-14 13:12:13 -0300

    first commit

---
+++ app/src/main/java/com/example/swipeaplication/SwipeRight.kt
@@ -0,0 +1,38 @@
+package com.example.swipeaplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+
+class SwipeRight : AppCompatActivity() {
+    var x1 = 0f
+    var x2 = 0f
+    var y1 = 0f
+    var y2 = 0f
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swipe_right)
+    }
+
+    override fun onTouchEvent(touchEvent: MotionEvent): Boolean {
+        when (touchEvent.action) {
+            MotionEvent.ACTION_DOWN -> {
+                x1 = touchEvent.x
+                y1 = touchEvent.y
+            }
+            MotionEvent.ACTION_UP -> {
+                x2 = touchEvent.x
+                y2 = touchEvent.y
+                if (x1 < x2) {
+                    val i = Intent(this@SwipeRight, MainActivity::class.java)
+                    startActivity(i)
+                } else if (x1 > x2) {
+                    val i = Intent(this@SwipeRight, SwipeLeft::class.java)
+                    startActivity(i)
+                }
+            }
+        }
+        return false
+    }
+}
